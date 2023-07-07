e932626f59b0d7b837cab0e216491794c36e2908
conghaiyang <2264032746@qq.com>
2023-07-07 15:09:37 +0800

    first commit

---
+++ src/utils/request.ts
@@ -0,0 +1,13 @@
+// let a = 10;
+
+test();
+
+function test() {
+  // let a = 1;
+  console.log("111");
+}
+
+// let fn = x => {
+//   console.log(x);
+// };
+// let obj = { id: 1, name: "zhangsan", age: 10 };
